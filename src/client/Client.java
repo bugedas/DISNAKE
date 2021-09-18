ae944a312334fa7b4dc4524838b38285f150ade0
Gediminas <bugedas@gmail.com>
2021-09-18 13:08:27 +0300

    Making CLient side to use Factories

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,27 +1,13 @@
 package client;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
-import java.util.Date;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 
 public class Client
 {
     public static void main(String[] args) throws Exception
     {
-        Socket socket = new Socket("127.0.0.1", 4000);
-        BufferedWriter writerChannel = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        BufferedReader readerChannel = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String line;
-
-        writerChannel.write(new Date().toString() + "\n\r");
-        writerChannel.flush();
-
-        while ((line = readerChannel.readLine()) != null)
-        {
-            System.out.println(line);
-        }
+        SSLSocketFactory socketBuilder = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) socketBuilder.createSocket("127.0.0.1", 4000);
     }
 }
