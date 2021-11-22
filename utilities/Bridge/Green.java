bea790298c9b9e3a0232c1d09a4c5761d66d59f4
donat <donatas.petrikauskas@gmail.com>
2021-11-22 18:12:52 +0200

    flyweightfactory

--- utilities/Bridge/Green.java
+++ utilities/Bridge/Green.java
@@ -1,12 +1,14 @@
 package utilities.Bridge;
+import interfaces.ObjectColorInterface;
+
 import java.awt.Color;
-public class Green extends ObjectColor {
+public class Green extends ObjectColor implements ObjectColorInterface {
     public Green(){
         super();
     }
+
     @Override
     public Color colorize() {
-
         return Color.GREEN;
     }
 }
--- utilities/Factory/ObjectColorFlyweightFactory.java
+++ utilities/Factory/ObjectColorFlyweightFactory.java
@@ -9,7 +9,6 @@ public class ObjectColorFlyweightFactory {
     static Map<String, ObjectColor> colors = new HashMap<>();
 
     public static ObjectColor getObjectColor(String name, ObjectColor color) {
-
         return colors.computeIfAbsent(name, k -> color);
     }
 }
