4a133b63e88cbee0ea8b1c880d18eea6bedd2a0e
luxis <luxisgrublys@gmail.com>
2021-12-07 17:53:11 +0200

    Iterator

---
+++ utilities/Iterator/Collection.java
@@ -0,0 +1,5 @@
+package utilities.Iterator;
+
+public interface Collection {
+    Iterator createIterator();
+}
---
+++ utilities/Iterator/Score.java
@@ -0,0 +1,13 @@
+package utilities.Iterator;
+
+public class Score {
+    short score;
+    public Score(short score)
+    {
+        this.score = score;
+    }
+    public short getScore()
+    {
+        return score;
+    }
+}
